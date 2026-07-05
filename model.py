ff48a82d39970a081acfc22173bde92f3821862f
jieruan <jieruan@didi-labs.com>
2026-07-04 21:51:40 -0500

    Add class placeholder for main LM to implement.

---
+++ model.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+
+
+class Transfomer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class T5(nn.Module):
+    def __init__(self):
+        pass
+
+
+class BERT(nn.Module):
+    def __init__(self):
+        pass
+
+
+class GPT1(nn.Module):
+    def __init__(self):
+        pass
+
+
+class GPT2(nn.Module):
+    def __init__(self):
+        pass
+
+
+class GPT3(nn.Module):
+    def __init__(self):
+        pass
+
+
+class LLaMA1(nn.Module):
+    def __init__(self):
+        pass
+
+
+class LLaMA2(nn.Module):
+    def __init__(self):
+        pass
+
+
+class LLaMA3(nn.Module):
+    def __init__(self):
+        pass
+
+
+class PaLM(nn.Module):
+    def __init__(self):
+        pass
+
+
+class DeepSeekV2(nn.Module):
+    def __init__(self):
+        pass
+
+
+class DeepSeekV3(nn.Module):
+    def __init__(self):
+        pass
+
+
+class DeepSeekV4(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Mistral(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Mixtral(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Mamba(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Chinchilla(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Gopher(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Qwen(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Qwen2(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Qwen3(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Gemma(nn.Module):
+    def __init__(self):
+        pass
