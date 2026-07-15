768e3c1b81ed7ec63e7d40377df99fec4ab1f95f
jieruan <jieruan@didi-labs.com>
2026-07-15 14:10:29 -0500

    Complete GPT3.

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import copy
 import math
 import torch
 import torch.nn as nn
@@ -154,9 +155,13 @@ class GPT2(nn.Module):
 
 
 class GPT3(nn.Module):
-    VOCAB_SIZE = 0
-    MODEL_DIM = 0
-    CONTEXT_WINDOW = 0
+    VOCAB_SIZE = 50257
+    CONTEXT_WINDOW = 2048
+    MODEL_DIM = 12288
+    NUM_HEADS = 96
+    DIM_FEEDFORWARD = 4 * MODEL_DIM
+    NUM_LAYERS = 96
+    SPARSE_BAND = 128
 
     def __init__(self, device=None, dtype=None):
         super().__init__()
@@ -168,13 +173,67 @@ class GPT3(nn.Module):
             torch.empty(self.CONTEXT_WINDOW, self.MODEL_DIM, **config)
         )
         self.dropout = nn.Dropout(p=0.1)
-        self.encoder = nn.ModuleList
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=self.MODEL_DIM,
+            nhead=self.NUM_HEADS,
+            dim_feedforward=self.DIM_FEEDFORWARD,
+            dropout=0.1,
+            activation=nn.functional.gelu,
+            batch_first=True,
+            norm_first=True,
+            **config,
+        )
+        self.encoder = nn.ModuleList(
+            [copy.deepcopy(encoder_layer) for _ in range(self.NUM_LAYERS)]
+        )
+        self.layer_norm = nn.LayerNorm(self.MODEL_DIM, **config)
+        self.register_buffer(
+            "dense_mask",
+            nn.Transformer.generate_square_subsequent_mask(
+                self.CONTEXT_WINDOW, **config
+            ),
+            persistent=False,
+        )
+        self.register_buffer(
+            "sparse_mask",
+            nn.Transformer.generate_square_subsequent_mask(
+                self.CONTEXT_WINDOW, **config
+            )
+            + torch.full(
+                (self.CONTEXT_WINDOW, self.CONTEXT_WINDOW), -torch.inf, **config
+            ).tril_(diagonal=-self.SPARSE_BAND),
+            persistent=False,
+        )
+        self.reset_parameters()
 
     def forward(self, input):
-        pass
+        length = input.size(-1)
+        assert length <= self.CONTEXT_WINDOW
+        out = self.dropout(self.embedding(input) + self.position_embedding[:length, :])
+        for i, layer in enumerate(self.encoder):
+            mask = self.dense_mask if i % 2 == 0 else self.sparse_mask
+            out = layer(out, src_mask=mask[:length, :length], is_causal=True)
+        out = self.layer_norm(out)
+        out = out @ self.embedding.weight.T
+        return out
 
     def reset_parameters(self):
-        pass
+        nn.init.normal_(self.position_embedding, std=0.02)
+        for module in self.modules():
+            if isinstance(module, nn.Embedding):
+                nn.init.normal_(module.weight, std=0.02)
+            elif isinstance(module, nn.Linear):
+                nn.init.normal_(module.weight, std=0.02)
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.MultiheadAttention):
+                nn.init.normal_(module.in_proj_weight, std=0.02)
+                nn.init.zeros_(module.in_proj_bias)
+
+        residual_layer_scaler = 1.0 / math.sqrt(2 * self.NUM_LAYERS)
+        for name, param in self.encoder.named_parameters():
+            if name.endswith("out_proj.weight") or name.endswith("linear2.weight"):
+                nn.init.normal_(param, std=0.02 * residual_layer_scaler)
 
 
 class LLaMA1(nn.Module):
