f0715c30071a654e557144185cbd6a1a83b2f8f3
jieruan <jieruan@didi-labs.com>
2026-07-28 10:02:34 -0500

    Refactor GPT series.

--- model.py
+++ model.py
@@ -275,12 +275,12 @@ class BERT(nn.Module):
 
 
 class GPTBase(nn.Module):
-    VOCAB_SIZE = 0
-    CONTEXT_WINDOW = 0
-    MODEL_DIM = 0
-    NUM_HEADS = 0
-    DIM_FEEDFORWARD = 0
-    NUM_LAYERS = 0
+    VOCAB_SIZE = 40478
+    CONTEXT_WINDOW = 512
+    MODEL_DIM = 768
+    NUM_HEADS = 12
+    DIM_FEEDFORWARD = 4 * MODEL_DIM
+    NUM_LAYERS = 12
     DROPOUT = 0.1
     LAYER_NORM_EPS = 1e-05
     SPARSE_BAND = 128
@@ -294,17 +294,19 @@ class GPTBase(nn.Module):
         dtype=None,
     ):
         super().__init__()
+        self.norm_first = norm_first
+
         config = {"device": device, "dtype": dtype}
         self.embedding = nn.Embedding(
-            num_embeddings=self.VOCAB_Size, embedding_dim=self.MODEL_DIM, **config
+            num_embeddings=self.VOCAB_SIZE, embedding_dim=self.MODEL_DIM, **config
         )
         self.position_embedding = nn.Parameter(
             torch.empty(self.CONTEXT_WINDOW, self.MODEL_DIM, **config)
         )
         self.dropout = nn.Dropout(p=self.DROPOUT)
-        encoder_layer = encoder_layer = nn.TransformerEncoderLayer(
+        encoder_layer = nn.TransformerEncoderLayer(
             d_model=self.MODEL_DIM,
-            num_heads=self.NUM_HEADS,
+            nhead=self.NUM_HEADS,
             dim_feedforward=self.DIM_FEEDFORWARD,
             dropout=self.DROPOUT,
             activation=nn.functional.gelu,
@@ -314,7 +316,7 @@ class GPTBase(nn.Module):
             **config,
         )
         self.encoder = nn.ModuleList(
-            [copy.deppcopy(encoder_layer) for _ in range(self.NUM_LAYERS)]
+            [copy.deepcopy(encoder_layer) for _ in range(self.NUM_LAYERS)]
         )
         self.register_buffer(
             "dense_mask",
@@ -327,7 +329,7 @@ class GPTBase(nn.Module):
             "sparse_mask",
             self.dense_mask
             + torch.full(
-                (self.CONTEXT_WINDON, self.CONTEX_WINDOW), -torch.inf, **config
+                (self.CONTEXT_WINDOW, self.CONTEXT_WINDOW), -torch.inf, **config
             ).tril_(diagonal=-self.SPARSE_BAND)
             if alternate_sparse_attn
             else None,
@@ -338,7 +340,8 @@ class GPTBase(nn.Module):
                 self.MODEL_DIM, eps=self.LAYER_NORM_EPS, bias=True, **config
             )
         else:
-            self.register_parameter("layer_norm", None)
+            self.register_module("layer_norm", None)
+        self.reset_parameters()
 
     def forward(self, input):
         length = input.size(-1)
@@ -347,69 +350,14 @@ class GPTBase(nn.Module):
         out = self.embedding(input) + self.position_embedding[:length, :]
         out = self.dropout(out)
         for i, layer in enumerate(self.encoder):
-            is_sparse = i % 2 == 1 and self.sparse_mask is not None
-            mask = self.sparse_mask if is_sparse else self.dense_mask
-            out = layer(out, src_mask=mask[:length, :length], is_causal=True)
+            is_dense = i % 2 == 0 or self.sparse_mask is None
+            mask = self.dense_mask if is_dense else self.sparse_mask
+            out = layer(out, src_mask=mask[:length, :length], is_causal=is_dense)
         if self.layer_norm is not None:
             out = self.layer_norm(out)
         out = out @ self.embedding.weight.T
         return out
 
-    def reset_parameters():
-        pass
-
-
-class GPT1(nn.Module):
-    # BPE with 40000 merges
-    VOCAB_SIZE = 40478
-    CONTEXT_WINDOW = 512
-    MODEL_DIM = 768
-    NUM_HEADS = 12
-    DIM_FEEDFORWARD = 3072
-    NUM_LAYERS = 12
-
-    def __init__(self, device=None, dtype=None):
-        super().__init__()
-        config = {"device": device, "dtype": dtype}
-        self.embedding = nn.Embedding(
-            num_embeddings=self.VOCAB_SIZE, embedding_dim=self.MODEL_DIM, **config
-        )
-        self.position_embedding = nn.Parameter(
-            torch.empty(self.CONTEXT_WINDOW, self.MODEL_DIM, **config)
-        )
-        self.dropout = nn.Dropout(p=0.1)
-        self.encoder = nn.TransformerEncoder(
-            encoder_layer=nn.TransformerEncoderLayer(
-                d_model=self.MODEL_DIM,
-                nhead=self.NUM_HEADS,
-                dim_feedforward=self.DIM_FEEDFORWARD,
-                dropout=0.1,
-                activation=nn.functional.gelu,
-                layer_norm_eps=1e-05,
-                batch_first=True,
-                norm_first=False,
-                **config,
-            ),
-            num_layers=self.NUM_LAYERS,
-        )
-        self.register_buffer(
-            "mask",
-            nn.Transformer.generate_square_subsequent_mask(
-                self.CONTEXT_WINDOW, device=device
-            ),
-            persistent=False,
-        )
-
-        self.reset_parameters()
-
-    def forward(self, input):
-        length = input.size(-1)
-        assert length <= self.CONTEXT_WINDOW
-        out = self.dropout(self.embedding(input) + self.position_embedding[:length, :])
-        out = self.encoder(out, mask=self.mask[:length, :length], is_causal=True)
-        out = out @ self.embedding.weight.T
-        return out
-
     def reset_parameters(self):
         nn.init.normal_(self.position_embedding, std=0.02)
         for module in self.modules():
@@ -417,164 +365,59 @@ class GPT1(nn.Module):
                 nn.init.normal_(module.weight, std=0.02)
             elif isinstance(module, nn.Linear):
                 nn.init.normal_(module.weight, std=0.02)
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
+                nn.init.zeros_(module.bias)
+            # need additional branch to cover MHA as it is not covered by Linear.
             elif isinstance(module, nn.MultiheadAttention):
                 nn.init.normal_(module.in_proj_weight, std=0.02)
                 nn.init.zeros_(module.in_proj_bias)
 
+        res_out_std = 0.02 / math.sqrt(2 * self.NUM_LAYERS)
+        if self.norm_first:
+            for name, param in self.encoder.named_parameters():
+                if name.endswith("out_proj.weight") or name.endswith("linear2.weight"):
+                    nn.init.normal_(param, std=res_out_std)
 
-class GPT2(nn.Module):
+
+class GPT1(GPTBase):
+    def __init__(self, device=None, dtype=None):
+        super().__init__(
+            norm_first=False,
+            final_layer_norm=False,
+            alternate_sparse_attn=False,
+            device=device,
+            dtype=dtype,
+        )
+
+
+class GPT2(GPTBase):
     VOCAB_SIZE = 50257
     CONTEXT_WINDOW = 1024
-    MODEL_DIM = 768
-    NUM_HEADS = 12
-    DIM_FEEDFORWARD = 3072
-    NUM_LAYERS = 12
 
     def __init__(self, device=None, dtype=None):
-        super().__init__()
-        config = {"device": device, "dtype": dtype}
-        self.embedding = nn.Embedding(
-            num_embeddings=self.VOCAB_SIZE, embedding_dim=self.MODEL_DIM, **config
+        super().__init__(
+            norm_first=True,
+            final_layer_norm=True,
+            alternate_sparse_attn=False,
+            device=device,
+            dtype=dtype,
         )
-        self.position_embedding = nn.Parameter(
-            torch.empty(self.CONTEXT_WINDOW, self.MODEL_DIM, **config)
-        )
-        self.dropout = nn.Dropout(p=0.1)
-        self.encoder = nn.TransformerEncoder(
-            encoder_layer=nn.TransformerEncoderLayer(
-                d_model=self.MODEL_DIM,
-                nhead=self.NUM_HEADS,
-                dim_feedforward=self.DIM_FEEDFORWARD,
-                dropout=0.1,
-                activation=nn.functional.gelu,
-                layer_norm_eps=1e-05,
-                batch_first=True,
-                norm_first=True,
-                **config,
-            ),
-            num_layers=self.NUM_LAYERS,
-        )
-        self.layer_norm = nn.LayerNorm(self.MODEL_DIM, eps=1e-05, bias=True, **config)
-        self.register_buffer(
-            "mask",
-            nn.Transformer.generate_square_subsequent_mask(
-                self.CONTEXT_WINDOW, device=device
-            ),
-            persistent=False,
-        )
-        self.reset_parameters()
-
-    def forward(self, input):
-        length = input.size(-1)
-        assert length <= self.CONTEXT_WINDOW
-        out = self.dropout(self.embedding(input) + self.position_embedding[:length, :])
-        out = self.encoder(out, mask=self.mask[:length, :length], is_causal=True)
-        out = self.layer_norm(out)
-        out = out @ self.embedding.weight.T
-        return out
-
-    def reset_parameters(self):
-        nn.init.normal_(self.position_embedding, std=0.02)
-        for module in self.modules():
-            if isinstance(module, nn.Embedding):
-                nn.init.normal_(module.weight, std=0.02)
-            elif isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, std=0.02)
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
-            elif isinstance(module, nn.MultiheadAttention):
-                nn.init.normal_(module.in_proj_weight, std=0.02)
-                nn.init.zeros_(module.in_proj_bias)
-
-        res_layer_scaler = 1.0 / math.sqrt(self.NUM_LAYERS * 2)
-        for name, param in self.encoder.named_parameters():
-            if name.endswith("out_proj.weight") or name.endswith("linear2.weight"):
-                nn.init.normal_(param, std=0.02 * res_layer_scaler)
 
 
-class GPT3(nn.Module):
+class GPT3(GPTBase):
     VOCAB_SIZE = 50257
     CONTEXT_WINDOW = 2048
     MODEL_DIM = 12288
     NUM_HEADS = 96
-    DIM_FEEDFORWARD = 4 * MODEL_DIM
     NUM_LAYERS = 96
-    SPARSE_BAND = 128
 
     def __init__(self, device=None, dtype=None):
-        super().__init__()
-        config = {"device": device, "dtype": dtype}
-        self.embedding = nn.Embedding(
-            num_embeddings=self.VOCAB_SIZE, embedding_dim=self.MODEL_DIM, **config
-        )
-        self.position_embedding = nn.Parameter(
-            torch.empty(self.CONTEXT_WINDOW, self.MODEL_DIM, **config)
-        )
-        self.dropout = nn.Dropout(p=0.1)
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=self.MODEL_DIM,
-            nhead=self.NUM_HEADS,
-            dim_feedforward=self.DIM_FEEDFORWARD,
-            dropout=0.1,
-            activation=nn.functional.gelu,
-            batch_first=True,
+        super().__init__(
             norm_first=True,
-            **config,
+            final_layer_norm=True,
+            alternate_sparse_attn=True,
+            device=device,
+            dtype=dtype,
         )
-        self.encoder = nn.ModuleList(
-            [copy.deepcopy(encoder_layer) for _ in range(self.NUM_LAYERS)]
-        )
-        self.layer_norm = nn.LayerNorm(self.MODEL_DIM, **config)
-        self.register_buffer(
-            "dense_mask",
-            nn.Transformer.generate_square_subsequent_mask(
-                self.CONTEXT_WINDOW, **config
-            ),
-            persistent=False,
-        )
-        self.register_buffer(
-            "sparse_mask",
-            nn.Transformer.generate_square_subsequent_mask(
-                self.CONTEXT_WINDOW, **config
-            )
-            + torch.full(
-                (self.CONTEXT_WINDOW, self.CONTEXT_WINDOW), -torch.inf, **config
-            ).tril_(diagonal=-self.SPARSE_BAND),
-            persistent=False,
-        )
-        self.reset_parameters()
-
-    def forward(self, input):
-        length = input.size(-1)
-        assert length <= self.CONTEXT_WINDOW
-        out = self.dropout(self.embedding(input) + self.position_embedding[:length, :])
-        for i, layer in enumerate(self.encoder):
-            is_dense = i % 2 == 0
-            mask = self.dense_mask if is_dense else self.sparse_mask
-            out = layer(out, src_mask=mask[:length, :length], is_causal=is_dense)
-        out = self.layer_norm(out)
-        out = out @ self.embedding.weight.T
-        return out
-
-    def reset_parameters(self):
-        nn.init.normal_(self.position_embedding, std=0.02)
-        for module in self.modules():
-            if isinstance(module, nn.Embedding):
-                nn.init.normal_(module.weight, std=0.02)
-            elif isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, std=0.02)
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
-            elif isinstance(module, nn.MultiheadAttention):
-                nn.init.normal_(module.in_proj_weight, std=0.02)
-                nn.init.zeros_(module.in_proj_bias)
-
-        res_layer_scaler = 1.0 / math.sqrt(2 * self.NUM_LAYERS)
-        for name, param in self.encoder.named_parameters():
-            if name.endswith("out_proj.weight") or name.endswith("linear2.weight"):
-                nn.init.normal_(param, std=0.02 * res_layer_scaler)
 
 
 class LLaMA1(nn.Module):
